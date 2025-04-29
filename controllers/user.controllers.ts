9cdc243fef9fb68994cb8e7d7e3c9a11994742ce
adarsh0707-kumar <adarshku123456789@gmail.com>
2025-04-30 04:42:55 +0530

    Login User

--- controllers/user.controllers.ts
+++ controllers/user.controllers.ts
@@ -7,6 +7,7 @@ require("dotenv").config();
 import ejs from 'ejs';
 import path from 'path';
 import sendMail from "../utils/sendMail";
+import { resolveTypeReferenceDirective } from "typescript";
 
 
 
@@ -142,9 +143,38 @@ export const activateUser = CatchAsyncError(async (req: Request, res: Response, 
 
 // Login user
 
+interface ILoginRequest {
+  email: string;
+  password: string;
+}
+
+export const loginUser = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body as ILoginRequest;
+
+    if (!email || !password) {
+      return next(new ErrorHandler("Please enter email and password", 400))
+    };
+
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (!user) {
+      return next(new ErrorHandler("Invalid email or password", 400))
+    };
+
+    const isPasswordMatch = await user.comparePassword(password);
+
+    if (!isPasswordMatch) {
+      return next(new ErrorHandler("Invalid email or password", 400));
+    }
 
 
 
+  }
+  catch (err: any) {
+    return next(new ErrorHandler(err.message, 400));
+  }
+})
 
 
 
